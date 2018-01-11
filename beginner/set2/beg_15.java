0b4bd26be34764745e55a0d7709ced1cdad5b0ad
antaramishra <35274912+antaramishra@users.noreply.github.com>
2018-01-12 01:18:20 +0530

    beg15
    beg15

---
+++ beginner/set2/beg_15.java
@@ -0,0 +1,17 @@
+import java.io.Console;
+class beg_15
+{
+public static void main(String [] args)
+{
+Console c=System.console();
+int x=Integer.parseInt(c.readLine("enter 1st no.\n"));
+int y=Integer.parseInt(c.readLine("enter 2nd no.\n"));
+for(int i=x+1;i<y;i++)
+{
+	if(i%2==0)
+	{
+		System.out.print(i+" ");
+	}
+}
+}
+}
