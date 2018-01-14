263ca32149a5ce063f4cbcede72415a4897f8c13
antaramishra <35274912+antaramishra@users.noreply.github.com>
2018-01-14 13:29:01 +0530

    29
    29

---
+++ beginner/set3/beg_29.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class beg_29
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the time in minutes :");
+int min=sc.nextInt();
+int hr=0,min1=0;
+if(min<60)
+{
+	min1=min;
+}
+else if(min>60)
+{
+	hr=min/60;
+	min1=min%60;
+}
+System.out.println(hr+":"+min1);
+}
+}
+
+ 
