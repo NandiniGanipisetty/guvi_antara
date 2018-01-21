84ab29edbf55d9e8eb06e9caefb36b1476884a10
antaramishra <35274912+antaramishra@users.noreply.github.com>
2018-01-21 22:27:11 +0530

    31
    31

---
+++ beginner/set4/beg_31.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class beg_31
+{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+char c;
+int z=0;
+String x=sc.nextLine();
+for(int i=0;i<x.length();i++)
+{
+	c=x.charAt(i);
+	if((int)c!=32)
+	{
+		z++;
+	}		
+}
+System.out.println(z);
+}
+}
+		
+
