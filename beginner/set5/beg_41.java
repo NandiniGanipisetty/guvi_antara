c53e8ccb87759f5e15eaae4438a09d6542f4f263
antaramishra <35274912+antaramishra@users.noreply.github.com>
2018-01-29 22:51:31 +0530

    41
    41

---
+++ beginner/set5/beg_41.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class beg_41
+{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+int x=sc.nextInt();
+for(int i=0;i<x;i++)
+{
+	System.out.println(s);
+}
+}
+}
