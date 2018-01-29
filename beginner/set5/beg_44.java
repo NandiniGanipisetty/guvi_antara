6de9ce0152623f375fe1d4ae07658035c09dc13c
antaramishra <35274912+antaramishra@users.noreply.github.com>
2018-01-29 23:32:35 +0530

    44
    44

---
+++ beginner/set5/beg_44.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class beg_44
+{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+int x=sc.nextInt();
+if(x >=1 && x<=10)
+{
+	System.out.println("yes");
+}
+else
+{
+	System.out.println("no");
+}
+}
+}
