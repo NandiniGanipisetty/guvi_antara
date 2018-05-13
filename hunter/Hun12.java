f01aecebc8cb8930e6d2d3c6654c248a521d6d53
antaramishra <35274912+antaramishra@users.noreply.github.com>
2018-05-13 15:06:34 +0530

    12
    12

---
+++ hunter/Hun12.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Hun12
+{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int k=sc.nextInt();
+int []a=new int[n];
+for(int i=0;i<n;i++)
+{
+	a[i]=sc.nextInt();
+}
+int temp=0;
+for(int i=0;i<n-1;i++)
+{
+	for(int j=0;j<n-i-1;j++)
+	{
+		if(a[j]<a[j+1])
+		{
+			temp=a[j]; 
+			a[j]=a[j+1];
+			a[j+1]=temp;
+		}
+	}
+}
+System.out.println(a[k-1]);
+}
+}
+
+
