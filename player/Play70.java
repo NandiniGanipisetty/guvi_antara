5bdf36712fa7a4fcd26577651cd85dce7c21c0b2
antaramishra <35274912+antaramishra@users.noreply.github.com>
2018-06-22 22:39:53 +0530

    d
    d

---
+++ player/Play70.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Play70
+{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+String x=sc.nextLine();
+StringBuilder sb=new StringBuilder(x);
+System.out.println(sb.append(" Answer"));
+}
+}
